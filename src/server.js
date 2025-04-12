cc87d6cc414d310e9c20a1b23966590809185de9
daehyeong2 <daehyeong2010@naver.com>
2025-04-12 23:02:16 +0900

    update

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import logger from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
@@ -10,6 +11,15 @@ app.use(logger("dev"));
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
