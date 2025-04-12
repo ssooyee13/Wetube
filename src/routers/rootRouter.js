74452ded94470a5781c9a0fbf6a551b55c0cfeb3
daehyeong2 <daehyeong2010@naver.com>
2025-04-12 16:04:01 +0900

    update

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,12 +1,17 @@
 import express from "express";
-import { getJoin, postJoin, login } from "../controllers/usercontroller";
+import {
+  getJoin,
+  postJoin,
+  getLogin,
+  postLogin,
+} from "../controllers/usercontroller";
 import { home, search } from "../controllers/videocontroller";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
 rootRouter.route("/join").get(getJoin).post(postJoin);
-rootRouter.get("/login", login);
+rootRouter.route("/login").get(getLogin).post(postLogin);
 rootRouter.get("/search", search);
 
 export default rootRouter;
